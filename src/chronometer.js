d90733f7fa3313e0425cb4c4526e06bac3ad60eb
songa1 <songachillethe1her@gmail.com>
2023-03-26 19:48:50 +0200

    first commit

---
+++ src/chronometer.js
@@ -0,0 +1,56 @@
+class Chronometer {
+  constructor() {
+    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+  }
+
+  start(printTimeCallback) {
+    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback()
+      };
+    }, 1000);
+  }
+
+  getMinutes() {
+    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
+  }
+
+  getSeconds() {
+    // ... your code goes here
+    return this.currentTime % 60;
+  }
+
+  computeTwoDigitNumber(value) { // 7
+    // ... your code goes here
+    let strNum  = String(value); // "7"
+
+    if(strNum.length === 2){
+      return strNum;
+    }else{
+      return "0"+strNum; // "07"
+    }
+  }
+
+  stop() {
+    // ... your code goes here
+    clearInterval(this.intervalId)
+  }
+
+  reset() {
+    // ... your code goes here
+    this.currentTime = 0;
+  }
+
+  split() {
+    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+
+    return `${minutes}:${seconds}`; // "mm:ss"
+  }
+}
